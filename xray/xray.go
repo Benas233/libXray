109337991b8cebf4c446dbfe6913eddd0b1135fe
Benas233 <benas@digiteka.lt>
2025-08-27 12:19:04 +0300

    Update xray.go test
    Update xray.go test

--- xray/xray.go
+++ xray/xray.go
@@ -1,13 +1,20 @@
 package xray
 
 import (
+	"context"
+	"fmt"
 	"os"
 	"runtime/debug"
+	"time"
 
 	"github.com/xtls/libxray/memory"
 	"github.com/xtls/xray-core/common/cmdarg"
 	"github.com/xtls/xray-core/core"
 	_ "github.com/xtls/xray-core/main/distro/all"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 // Constants for environment variables
@@ -38,7 +45,7 @@ func StartXray(configPath string) (*core.Instance, error) {
 func StartXrayFromJSON(configJSON string) (*core.Instance, error) {
 	// Convert JSON string to bytes
 	configBytes := []byte(configJSON)
-	
+
 	// Use core.StartInstance which can load configuration directly from bytes
 	server, err := core.StartInstance("json", configBytes)
 	if err != nil {
@@ -111,4 +118,29 @@ func StopXray() error {
 // Xray's version
 func XrayVersion() string {
 	return core.Version()
+}
+
+func QueryLasthandshake(server string) (string, error) {
+
+	conn, err := grpc.Dial(server, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return "", fmt.Errorf("failed to connect: %w", err)
+	}
+	defer conn.Close()
+	req := &emptypb.Empty{}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	resp := &LastHandshakeResponse{}
+	method := "/xray.app.lasthandshake.command.LasthandshakeService/GetLastHandshake"
+	if err := conn.Invoke(ctx, method, req, resp); err != nil {
+		return "", fmt.Errorf("RPC call failed: %w", err)
+	}
+
+	jsonResp, err := protojson.Marshal(resp)
+	if err != nil {
+		return "", fmt.Errorf("failed to marshal respone: %w", err)
+	}
+
+	return string(jsonResp), nil
 }
